0dccf227713880da7cdb79040f79757a041904a2
maxhu08 <contact@maxhu.dev>
2026-03-01 22:20:46 -0600

    feat(formatting): update formatting

--- src/options/scripts/ui.ts
+++ src/options/scripts/ui.ts
@@ -1,27 +1,16 @@
 import { getButton, getContainerAndTextarea, getElement } from "~/src/options/scripts/utils/ui-helpers";
 
-// prettier-ignore
 export const [rulesUrlsContainerEl, rulesUrlsTextareaEl] = getContainerAndTextarea("rules-urls");
 export const rulesUrlsHighlightEl = getElement<HTMLPreElement>("rules-urls-highlight");
-// prettier-ignore
 export const [hotkeysMappingsContainerEl, hotkeysMappingsTextareaEl] = getContainerAndTextarea("hotkeys-mappings");
 export const hotkeysMappingsHighlightEl = getElement<HTMLPreElement>("hotkeys-mappings-highlight");
 export const hotkeysHintsCharsetContainerEl = getElement<HTMLDivElement>("hotkeys-hints-charset-container");
 export const hotkeysHintsCharsetInputEl = getElement<HTMLInputElement>("hotkeys-hints-charset-input");
-export const [hotkeysHintsAvoidAdjacentPairsContainerEl, hotkeysHintsAvoidAdjacentPairsTextareaEl] =
-  getContainerAndTextarea("hotkeys-hints-avoid-adjacent-pairs");
-export const hotkeysHintsAvoidAdjacentPairsHighlightEl = getElement<HTMLPreElement>(
-  "hotkeys-hints-avoid-adjacent-pairs-highlight"
-);
-export const hotkeysHintsPreferredSearchLabelsContainerEl = getElement<HTMLDivElement>(
-  "hotkeys-hints-preferred-search-labels-container"
-);
-export const hotkeysHintsPreferredSearchLabelsInputEl = getElement<HTMLInputElement>(
-  "hotkeys-hints-preferred-search-labels-input"
-);
-export const hotkeysHintsShowActivationIndicatorCheckboxEl = getElement<HTMLInputElement>(
-  "hotkeys-hints-show-activation-indicator-checkbox"
-);
+export const [hotkeysHintsAvoidAdjacentPairsContainerEl, hotkeysHintsAvoidAdjacentPairsTextareaEl] = getContainerAndTextarea("hotkeys-hints-avoid-adjacent-pairs");
+export const hotkeysHintsAvoidAdjacentPairsHighlightEl = getElement<HTMLPreElement>("hotkeys-hints-avoid-adjacent-pairs-highlight");
+export const hotkeysHintsPreferredSearchLabelsContainerEl = getElement<HTMLDivElement>("hotkeys-hints-preferred-search-labels-container");
+export const hotkeysHintsPreferredSearchLabelsInputEl = getElement<HTMLInputElement>("hotkeys-hints-preferred-search-labels-input");
+export const hotkeysHintsShowActivationIndicatorCheckboxEl = getElement<HTMLInputElement>("hotkeys-hints-show-activation-indicator-checkbox");
 
 export const saveButtonEl = getButton("save");
 export const exportButtonEl = getButton("export");
--- src/popup/ui.ts
+++ src/popup/ui.ts
@@ -1,6 +1,5 @@
 import { getContainerAndTextarea, getElement } from "~/src/options/scripts/utils/ui-helpers";
 
-export const [popupRuleEditorContainerEl, popupRuleEditorTextareaEl] =
-  getContainerAndTextarea("popup-rule-editor");
+export const [popupRuleEditorContainerEl, popupRuleEditorTextareaEl] = getContainerAndTextarea("popup-rule-editor");
 export const popupRuleEditorHighlightEl = getElement<HTMLPreElement>("popup-rule-editor-highlight");
 export const popupStatusTextEl = getElement<HTMLParagraphElement>("popup-status-text");
