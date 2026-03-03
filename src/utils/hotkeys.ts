2c8e4f86256f0f763f27bfcf323b3485f70e1b62
maxhu08 <contact@maxhu.dev>
2026-03-02 23:49:14 -0600

    refactor(config): make default config more clear

--- src/utils/hotkeys.ts
+++ src/utils/hotkeys.ts
@@ -49,51 +49,48 @@ export const isActionName = (value: string): value is ActionName => {
   return VALID_ACTION_NAMES.has(value as ActionName);
 };
 
-export const DEFAULT_HOTKEY_MAPPINGS = [
-  // scroll
-  "j scroll-down",
-  "k scroll-up",
-  "h scroll-left",
-  "l scroll-right",
-  "d scroll-half-page-down",
-  "u scroll-half-page-up",
-  "gg scroll-to-top",
-  "G scroll-to-bottom",
+export const DEFAULT_HOTKEY_MAPPINGS = `# scroll
+j scroll-down
+k scroll-up
+h scroll-left
+l scroll-right
+d scroll-half-page-down
+u scroll-half-page-up
+gg scroll-to-top
+G scroll-to-bottom
 
-  // history
-  "H history-go-prev",
-  "L history-go-next",
+# hints
+f toggle-hints-current-tab
+F toggle-hints-new-tab
 
-  // tabs
-  "J tab-go-prev",
-  "K tab-go-next",
+# tab actions
+t create-new-tab
+x close-current-tab
+r reload-current-tab
+R reload-current-tab-hard
+J tab-go-prev
+K tab-go-next
+yt duplicate-current-tab
+W move-current-tab-to-new-window
 
-  // hints
-  "f toggle-hints-current-tab",
-  "F toggle-hints-new-tab",
+# clipboard
+yy yank-current-tab-url
 
-  // tab actions
-  "W move-current-tab-to-new-window",
-  "yt duplicate-current-tab",
-  "# t create-new-tab",
-  "# x close-current-tab",
-  "# r reload-current-tab",
-  "# R reload-current-tab-hard",
-
-  // clipboard
-  "yy yank-current-tab-url"
-].join("\n");
+# history
+H history-go-prev
+L history-go-next
+`.trim();
 
 export const DEFAULT_HINT_CHARSET = "sadfjklewcmupgh";
+
 export const DEFAULT_HINT_PREFERRED_SEARCH_LABELS = "kj kjf kjfd";
 
-export const DEFAULT_HINT_AVOID_ADJACENT_PAIRS = [
-  "# double letters",
-  "aa cc dd ee ff gg hh jj kk ll mm pp ss ww",
-  "",
-  "# same finger bigrams",
-  "cd ce dc de ec ed fg gf hj hm jh jm mh mj sw ws",
-  "",
-  "# other",
-  "aw sa wa wd dw kp pk kl lp"
-].join("\n");
+export const DEFAULT_HINT_AVOID_ADJACENT_PAIRS = `# double letters
+aa cc dd ee ff gg hh jj kk ll mm pp ss ww
+
+# same finger bigrams
+cd ce dc de ec ed fg gf hj hm jh jm mh mj sw ws
+
+# other
+aw sa wa wd dw kp pk kl lp
+`.trim();
